5bac4a65258978f00f3a39c6894bfde459f65430
Joshua Pawlicki <gilgarn@gmail.com>
2019-10-11 15:44:24 -0700

    Content tests, content reorganization

--- src/main/java/haus/rules/server/PageList.java
+++ src/main/java/haus/rules/server/PageList.java
@@ -2,15 +2,15 @@ package haus.rules;
 
 import com.google.gson.Gson;
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
-import java.util.ArrayList;
 
 final class PageList {
 	public static Map<String, Page> loadPages(String resource) throws IOException {
@@ -21,14 +21,17 @@ final class PageList {
 		return map;
 	}
 
-	private static void addMap(Entry node, Map<String, Page> map, ArrayList<Page.Spec> ancestors) {
+	private static void addMap(Entry node, Map<String, Page> map, ArrayList<Page.Spec> ancestors) throws IOException {
 		List<Page.Spec> childspecs = node.pages.stream().map(c -> new Page.Spec(c.title, c.url)).collect(Collectors.toList());
 		Page.Spec thisSpec = new Page.Spec(node.title, node.url);
 		ancestors.add(thisSpec);
 		String url = ancestors.stream().map(s -> s.url).collect(Collectors.joining("/"));
-		String text = new BufferedReader(new InputStreamReader(PageList.class.getResourceAsStream(url + "/_.md"), StandardCharsets.UTF_8)).lines().collect(Collectors.joining("\n"));
+		String text;
+		try (InputStream resource = PageList.class.getResourceAsStream(url + "/_.md")) {
+			if (resource == null) throw new IOException("Failed to open resource file: " + url + "/_.md");
+			text = new BufferedReader(new InputStreamReader(resource, StandardCharsets.UTF_8)).lines().collect(Collectors.joining("\n"));
+		}
 		if ("".equals(url)) url = "/";
-		System.out.println("Loaded " + url);
 		map.put(url, new Page(node.title, node.source, childspecs, ancestors, text));
 		for (Entry child : node.pages) addMap(child, map, ancestors);
 		ancestors.remove(thisSpec);
---
+++ src/test/java/haus/rules/server/PageListTest.java
@@ -0,0 +1,58 @@
+package haus.rules;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import org.junit.Test;
+
+public class PageListTest {
+	@Test public void testLoads() throws Exception {
+		assertNotNull(PageList.loadPages("/sitemap.json"));
+	}
+
+	@Test public void testDeadLinks() throws Exception {
+		Pattern linker = Pattern.compile("\\[.*?\\]\\((/.*?)\\)");
+		Map<String, Page> pages = PageList.loadPages("/sitemap.json");
+		List<String> errors = new ArrayList<>();
+		for (Map.Entry<String, Page> e : pages.entrySet()) {
+			Matcher m = linker.matcher(e.getValue().text);
+			while (m.find()) {
+				if (!pages.containsKey(m.group(1))) errors.add(e.getKey() + " has dead link to \"" + m.group(1) + "\"");
+			}
+		}
+		for (String s : errors) System.out.println(s);
+		assertTrue(errors.isEmpty());
+	}
+
+	@Test public void testSelfLinks() throws Exception {
+		Pattern linker = Pattern.compile("\\[.*?\\]\\((/.*?)\\)");
+		List<String> errors = new ArrayList<>();
+		for (Map.Entry<String, Page> e : PageList.loadPages("/sitemap.json").entrySet()) {
+			Matcher m = linker.matcher(e.getValue().text);
+			while (m.find()) {
+				if (m.group(1).equals(e.getKey())) errors.add(e.getKey() + " has a self-link");
+			}
+		}
+		for (String s : errors) System.out.println(s);
+		assertTrue(errors.isEmpty());
+	}
+
+	@Test public void testCommonTypos() throws Exception {
+		Map<String, String> typos = new HashMap<>();
+		List<String> errors = new ArrayList<>();
+		typos.put("role-playing", "roleplaying");
+		typos.put("table-top", "tabletop");
+		for (Map.Entry<String, Page> e : PageList.loadPages("/sitemap.json").entrySet()) {
+			for (Map.Entry<String, String> typo : typos.entrySet()) {
+				if (e.getValue().text.contains(typo.getKey())) errors.add(e.getKey() + " contains \"" + typo.getKey() + "\", use \"" + typo.getValue() + "\" instead");
+			}
+		}
+		for (String s : errors) System.out.println(s);
+		assertTrue(errors.isEmpty());
+	}
+}
